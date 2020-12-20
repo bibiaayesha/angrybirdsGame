39ff8378c83519d947020c446ed374b6839c5397
bibiaayesha <74051352+bibiaayesha@users.noreply.github.com>
2020-12-20 07:07:22 +0300

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,18 @@
+class Log {
+    constructor(x, y, height, angle) {
+      
+      Matter.Body.setAngle(this.body, angle);
+
+
+      super(x1,y1,20,height,angle);
+ 
+  this.image = loadImage("sprites/wood1.png");
+     
+  
+   
+   
+
+      
+    }
+  }
+  
